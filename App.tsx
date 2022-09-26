5b73834895ead82509b667a20c84b5585c4190d3
CliffCrerar <cliff.crerar@gmail.com>
2022-09-26 20:02:53 +0200

    2 files modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import { ScrollingContainer } from './components/ScrollingContainer';
 import './style.css';
+import demoData from './data/demo-data.json';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <ScrollingContainer heading="Scrolly Container" />
+      <ScrollingContainer heading="Scrolly Container" list={demoData} />
     </div>
   );
 }
--- components/ScrollingContainer.tsx
+++ components/ScrollingContainer.tsx
@@ -8,16 +8,32 @@ interface ScrollingContainerProps {
 export const ScrollingContainer: React.FC<ScrollingContainerProps> = (
   props: ScrollingContainerProps
 ) => {
+  // styles
+  console.log(props.list);
   const containerClasses = {
     border: '1px solid black',
   };
 
-  const scrollSectionClasses = {};
+  const scrollSectionClasses = {
+    background: 'red',
+    height: '10vh',
+    overflow: 'scroll',
+  };
+
+  // inherent children components
+  const ListComponent: React.FC = () => {
+    return props.list.map((listItem: any, idx: number) => {
+      return <li key={'list-item-' + idx}>{listItem.color}</li>;
+    });
+  };
+
   return (
     <div style={containerClasses}>
       {props.heading}
       <hr />
-      <div style={scrollSectionClasses}></div>
+      <ul style={scrollSectionClasses}>
+        <ListComponent />
+      </ul>
     </div>
   );
 };
